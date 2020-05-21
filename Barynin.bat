0d160b4f082f1fe75ca51d510201d6fbcd24a38e
borg3008 <51539191+borg3008@users.noreply.github.com>
2020-05-21 12:34:26 +0300

    Add files via upload

---
+++ Barynin.bat
@@ -0,0 +1,25 @@
+Echo off
+md Niclsson
+cd Niclsson
+md Jack
+cd Jack
+md Scott
+cd Scott
+pause
+echo > 12.txt
+cd .. 
+echo > Name.txt
+cd ..
+echo > 12122012.txt
+cd ..
+pause
+del Niclsson /S /Q /F
+pause
+cd Niclsson
+cd Jack
+rd Scott
+cd ..
+rd Jack
+cd ..
+rd  Niclsson
+pause
